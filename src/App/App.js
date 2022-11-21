3c25a6e1068ac51a8e4c202d0418a08a00000b62
Jolijn Rosendaal <84015050+Jolijn0101@users.noreply.github.com>
2022-11-21 13:24:09 +0100

    basics project page

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,12 @@
 import { useState } from 'react';
 import './App.css';
-import LoginPage from '../LoginPage/LoginPage.js';
+import LoginPage from '../LoginPage/LoginPage';
+import Project from '../Project/Project';
 
 function App() {
   const [logIn, setlogin] = useState('');
 
-  return (
-    <div className="App">
-      <LoginPage />
-    </div>
-  );
+  return <div className="App">{logIn === false ? <LoginPage /> : <Project />}</div>;
 }
 
 export default App;
---
+++ src/Project/Project.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import './Project.css';
+
+const Project = () => {
+  return (
+    <div className="project">
+      <h1>projectName</h1>
+      <ul className="project__todo-list">
+        <li className="project__todo">
+          <div className="project__check-box">
+            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <p>todo name</p>
+          <svg className="dots_ico" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <path
+              fill-rule="evenodd"
+              d="M3 9.5a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm5 0a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm5 0a1.5 1.5 0 110-3 1.5 1.5 0 010 3z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </li>
+        <li className="project__todo">
+          <div className="project__check-box">
+            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <p>todo name</p>
+          <svg className="dots_ico" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <path
+              fill-rule="evenodd"
+              d="M3 9.5a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm5 0a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm5 0a1.5 1.5 0 110-3 1.5 1.5 0 010 3z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </li>
+        <li className="project__todo">
+          <div className="project__check-box">
+            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <p>todo name</p>
+          <svg className="dots_ico" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <path
+              fill-rule="evenodd"
+              d="M3 9.5a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm5 0a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm5 0a1.5 1.5 0 110-3 1.5 1.5 0 010 3z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </li>
+      </ul>
+      <div className="project__add-todo">
+        <svg
+          className="project__plus-icon"
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          viewBox="0 0 1024 1024"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm192 472c0 4.4-3.6 8-8 8H544v152c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V544H328c-4.4 0-8-3.6-8-8v-48c0-4.4 3.6-8 8-8h152V328c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v152h152c4.4 0 8 3.6 8 8v48z"></path>
+        </svg>
+        <h4>add Todo</h4>
+      </div>
+      <div className="project__remove-project">
+        <svg
+          className="project__trash-icon"
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          viewBox="0 0 448 512"
+          height="1.8em"
+          width="1.8em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M268 416h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12zM432 80h-82.41l-34-56.7A48 48 0 0 0 274.41 0H173.59a48 48 0 0 0-41.16 23.3L98.41 80H16A16 16 0 0 0 0 96v16a16 16 0 0 0 16 16h16v336a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128h16a16 16 0 0 0 16-16V96a16 16 0 0 0-16-16zM171.84 50.91A6 6 0 0 1 177 48h94a6 6 0 0 1 5.15 2.91L293.61 80H154.39zM368 464H80V128h288zm-212-48h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12z"></path>
+        </svg>
+        <h4>remove project</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
